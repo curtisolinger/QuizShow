0903aebb42f94b1b5a04914d98251bd52ca2b6da
Curtis Olinger <curtisolinger@gmail.com>
2023-02-06 14:40:44 -0800

    Initial commit of the Quiz Show trivia program

---
+++ question_model.py
@@ -0,0 +1,16 @@
+# Define a Question class
+
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+# question_1 = Question("What is the capital of California", "Sacramento")
+
+# print(f"The first question is: {question_1.text}")
+# print(f"The answer is {question_1.answer}")
+
+# q_1 = Question(question_data[0]["text"], question_data[0]["answer"])
+
+# print(f"The first question is '{q_1.text}?'")
+# print(f"The answer is {q_1.answer}")

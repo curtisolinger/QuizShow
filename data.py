85385f6918ef01a6f6914dedcae9cca147da57f9
Curtis Olinger <curtisolinger@gmail.com>
2023-02-07 08:36:32 -0800

    Commented code

--- data.py
+++ data.py
@@ -21,39 +21,3 @@ question_data = [
     {"text": "A few ounces of chocolate can to kill a "
         "small dog.", "answer": "True"}
 ]
-
-
-# question_data = [
-#     {
-#         "category":"Science: Mathematics",
-#         "type":"boolean",
-#         "difficulty":"medium",
-#         "question":"111,111,111 x 111,111,111 = 12,345,678,987,654,321",
-#         "correct_answer":"True",
-#         "incorrect_answers":["False"]
-#     },
-#     {
-#         "category":"Science: Mathematics",
-#         "type":"boolean",
-#         "difficulty":"easy",
-#         "question":"The &#039;Squaring the Circle&#039; problem is solvable.",
-#         "correct_answer":"False",
-#         "incorrect_answers":["True"]
-#     },
-#     {
-#         "category":"Science: Mathematics",
-#         "type":"boolean",
-#         "difficulty":"hard",
-#         "question":"The binary number &quot;101001101&quot; is equivalent to the Decimal number &quot;334&quot;",
-#         "correct_answer":"False",
-#         "incorrect_answers":["True"]
-#     },
-#     {
-#         "category":"Science: Mathematics",
-#         "type":"boolean",
-#         "difficulty":"hard",
-#         "question":"If you could fold a piece of paper in half 50 times, its&#039; thickness will be 3/4th the distance from the Earth to the Sun.",
-#         "correct_answer":"True",
-#         "incorrect_answers":["False"],
-#     },
-# ]
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+# Import the Question class which contains two attributes: questions text and question answer
 from question_model import Question
+# Import a list of dictionaries that contain quiz questions and answers
 from data import question_data
+# Import the QuizBrain class that has three attributes: question number, question list, and the score
+# It also has three methods: still_has_question, 
 from quiz_brain import QuizBrain
 
+# Declare a list that will contain Question objects
 question_bank = []
 
 for question in question_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
+    # Extract the question from the dictionary
+    question_text = question["text"]
+    # Extract the answer from the dictionary
+    question_answer = question["answer"]
+    # Create a new Question object with the question text and answer
     new_question = Question(q_text=question_text, q_answer=question_answer)
+    # Append the new Question object to the question_bank list
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
